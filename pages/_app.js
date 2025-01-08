5d935dbc6cf2be448e11d3f6d60d9584e6fae630
PrasannaKumar999 <97685872+PrasannaKumar999@users.noreply.github.com>
2025-01-08 15:51:03 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import { CartProvider } from "@/context/cartContext";
+import Applayout from "@/layouts/applayout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	return (
+		<CartProvider>
+			<Applayout>
+				<Component {...pageProps} />
+			</Applayout>
+		</CartProvider>
+	);
 }
